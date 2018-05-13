8495f8a45dba5a9489ae931aa9ca687926e9232c
nami4628 <nami4628@cafe24.com>
2018-05-13 23:31:37 +0900

    .

--- script/editor.js
+++ script/editor.js
@@ -1,161 +1,4 @@
 window.onload = function () {
-  const editField = document.getElementById('edit-field');
-
-  editField.addEventListener('keypress', (e) => {
-    enterKey(e)
-  })
-
-  b.addEventListener('mousedown', () => {
-    bClick()
-  })
-
-  h2.addEventListener('mousedown', () => {
-    h2Click()
-  })
-
-  function enterKey(e) {
-    if (e.which !== 13) return true
-    if (!getIsLastCaret()) return true
-   console.log('test');
-    const p = document.createElement('p')
-    const br = document.createElement('br')
-    p.appendChild(br)
-    editField.appendChild(p)
-    setTimeout(function () {
-      placeCaretAtStart(p)
-    }, 0)
-
-    e.preventDefault()
-    return false
-  }
-
-  function bClick() {
-    const {
-      activeElement
-    } = document
-
-    if (activeElement === editField) {
-      const selection = document.getSelection()
-      const range = selection.getRangeAt(0)
-      const {
-        startContainer,
-        endContainer
-      } = range
-
-      if (startContainer === endContainer) {
-        range.surroundContents(document.createElement('b'))
-      }
-    }
-  }
-
-  function h2Click() {
-    const {
-      activeElement
-    } = document
-
-    if (activeElement === editField) {
-      const selection = document.getSelection()
-      const {
-        anchorNode,
-        type
-      } = selection
-      const anchorNodeType = anchorNode.nodeType
-
-      const h2 = document.createElement('h2')
-      const br = document.createElement('br')
-      h2.appendChild(br)
-
-      if (type === 'Caret') {
-        if (anchorNodeType === 1) {
-          activeElement.replaceChild(h2, anchorNode)
-        }
-
-        setTimeout(function () {
-          activeElement.focus()
-        }, 0)
-      }
-    }
-  }
-
-  function getIsLastCaret() {
-    let isLast = true
-
-    const selection = document.getSelection()
-    const range = selection.getRangeAt(0)
-    const rangeString = selection.toString()
-    const {
-      type,         // Caret, Range
-      anchorNode,   // 시작 노드
-      anchorOffset, // 시작 점
-      focusNode,    // 끝 노드
-      focusOffset   // 끝 점
-    } = selection
-
-    let lineElement = null
-    let elementWithAnchorNode = anchorNode
-    let tmpParentElement = anchorNode
-    for (;;) {
-      if (tmpParentElement.parentNode === editField) {
-        lineElement = tmpParentElement
-        break
-      }
-
-      elementWithAnchorNode = tmpParentElement
-      tmpParentElement = tmpParentElement.parentNode
-    }
-    
-    let elementWithFocusNode = focusNode
-    tmpParentElement = focusNode
-    for (;;) {
-      if (tmpParentElement.parentNode === editField) {
-        break
-      }
-
-      elementWithFocusNode = tmpParentElement
-      tmpParentElement = tmpParentElement.parentNode
-    }
-
-    /**
-     * type: Caret 
-     */
-    if (type === 'Caret') {
-      /**
-       * 우측 Siblings 객체가 있고, Siblings 객체 내부에 텍스트가 있을 경우 (isLast: false)
-       */
-      let tmpNextSibling = elementWithAnchorNode.nextSibling
-      for (;;) {
-        if (tmpNextSibling !== null) {
-          const {
-            textContent
-          } = tmpNextSibling
-          if (textContent !== '') {
-            isLast = false
-            break
-          }
-  
-          tmpNextSibling = tmpNextSibling.nextSibling
-        } else {
-          break
-        }
-      }
-
-      if (anchorOffset !== elementWithAnchorNode.textContent.length) {
-        isLast = false
-      }
-
-      return isLast
-    }
-    
-    /**
-     * type: Range 
-     */
-    if (type === 'Range') {
-
-    }
-
-    return isLast
-  }
-
   function createCaretPlacer(atStart) {
     return function (el) {
       el.focus();
@@ -178,4 +21,210 @@ window.onload = function () {
 
   var placeCaretAtStart = createCaretPlacer(true);
   var placeCaretAtEnd = createCaretPlacer(false);
+
+  function getCaretInfo() {
+    const selection = document.getSelection()
+    const range = selection.getRangeAt(0)
+    const rangeString = selection.toString()
+    const {
+      type, // Caret, Range
+      anchorNode, // 시작 노드
+      anchorOffset, // 시작 점
+      focusNode, // 끝 노드
+      focusOffset // 끝 점
+    } = selection
+
+    let lineElement = null // 라인 DOM
+    let elementWithAnchorNode = anchorNode // 라인 DOM 바로 하위 자식(출발을 포함한)
+    let tmpParentElement = anchorNode
+    for (;;) {
+      if (tmpParentElement.parentNode === editField) {
+        lineElement = tmpParentElement
+        break
+      }
+
+      elementWithAnchorNode = tmpParentElement
+      tmpParentElement = tmpParentElement.parentNode
+    }
+
+    let elementWithFocusNode = focusNode // 라인 DOM 바로 하위 자식(끝을 포함한)
+    tmpParentElement = focusNode
+    for (;;) {
+      if (tmpParentElement.parentNode === editField) {
+        break
+      }
+
+      elementWithFocusNode = tmpParentElement
+      tmpParentElement = tmpParentElement.parentNode
+    }
+
+    let prevElement = elementWithAnchorNode
+    let startCaret = anchorOffset
+    for (;;) {
+      if (prevElement.previousSibling === null)
+        break;
+
+      prevElement = prevElement.previousSibling
+      startCaret += prevElement.textContent.length
+    }
+
+    prevElement = elementWithFocusNode
+    let endCaret = focusOffset
+    for (;;) {
+      if (prevElement.previousSibling === null)
+        break;
+
+      prevElement = prevElement.previousSibling
+      endCaret += prevElement.textContent.length
+    }
+
+    let tmp = startCaret
+    startCaret = startCaret > endCaret ? endCaret : startCaret
+    endCaret = tmp > endCaret ? tmp : endCaret
+
+    let result = {
+      type,
+      range,
+      selection,
+      anchorOffset,
+      elementWithAnchorNode,
+      focusOffset,
+      elementWithFocusNode,
+      startCaret,
+      endCaret,
+      rangeString,
+      lineElement,
+      isLast: lineElement.textContent.length === (startCaret > endCaret ? startCaret : endCaret),
+      lineTextLength: lineElement.textContent.length
+    }
+
+    return result
+  }
+
+
+
+
+  const editField = document.getElementById('edit-field')
+
+  editField.addEventListener('keypress', (e) => enterKey(e))
+  b.addEventListener('mousedown', () => bClick())
+  h2.addEventListener('mousedown', () => hClick('h2'))
+  h3.addEventListener('mousedown', () => hClick('h3'))
+  h4.addEventListener('mousedown', () => hClick('h4'))
+  pre.addEventListener('mousedown', () => preClick())
+
+  function enterKey(e) {
+    if (e.which !== 13 || e.shiftKey) return true
+
+    const caretInfo = getCaretInfo();
+    const {
+      type,
+      isLast,
+      range,
+      lineElement
+    } = caretInfo
+    if (!isLast) return true
+
+    if (type === 'Range' && isLast) {
+      range.deleteContents()
+    }
+
+    const p = document.createElement('p')
+    const br = document.createElement('br')
+    p.appendChild(br)
+    lineElement.after(p)
+    setTimeout(function () {
+      placeCaretAtStart(p)
+    }, 0)
+
+    e.preventDefault()
+    return false
+  }
+
+  function bClick() {
+    const {
+      activeElement
+    } = document
+
+    if (activeElement === editField) {
+      const {
+        selection,
+        range,
+        elementWithAnchorNode,
+        elementWithFocusNode
+      } = getCaretInfo()
+      const {
+        startContainer,
+        endContainer
+      } = range
+
+      if (startContainer === endContainer) {
+        range.surroundContents(document.createElement('b'))
+      } else {
+        debugger;
+      }
+    }
+  }
+
+  function hClick(name) {
+    const {
+      activeElement
+    } = document
+
+    if (activeElement === editField) {
+      const selection = document.getSelection()
+      const {
+        anchorNode,
+        type
+      } = selection
+      const anchorNodeType = anchorNode.nodeType
+
+      const h = document.createElement(name)
+      const br = document.createElement('br')
+      h.appendChild(br)
+
+      if (type === 'Caret') {
+        if (anchorNodeType === 1) {
+          activeElement.replaceChild(h, anchorNode)
+        }
+
+        setTimeout(function () {
+          activeElement.focus()
+        }, 0)
+      }
+    }
+  }
+
+  function preClick() {
+    const {
+      activeElement
+    } = document
+
+    if (activeElement !== editField) return false
+
+    const selection = document.getSelection()
+    const {
+      anchorNode,
+      type
+    } = selection
+    const anchorNodeType = anchorNode.nodeType
+
+    const p = document.createElement('p')
+    const pre = document.createElement('pre')
+    const code = document.createElement('code')
+    const br = document.createElement('br')
+    p.appendChild(pre)
+    pre.appendChild(code)
+    code.appendChild(br)
+
+    if (type === 'Caret') {
+      if (anchorNodeType === 1) {
+        activeElement.replaceChild(pre, anchorNode)
+      }
+
+      setTimeout(function () {
+        activeElement.focus()
+      }, 0)
+    }
+  }
 }
