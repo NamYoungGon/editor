57926ce0ad822a8bd8f72e5cf35aa0577846c867
nyg4628 <nyg4628@gmail.com>
2018-05-16 18:10:54 +0900

    .

--- script/editor.js
+++ script/editor.js
@@ -5,23 +5,23 @@ function createCaretPlacer(atStart) {
     el.focus();
     if (typeof window.getSelection !== "undefined" &&
       typeof document.createRange !== "undefined") {
-      var range = document.createRange();
-      range.selectNodeContents(el);
-      range.collapse(atStart);
-      var sel = window.getSelection();
-      sel.removeAllRanges();
-      sel.addRange(range);
+      var range = document.createRange()
+      range.selectNodeContents(el)
+      range.collapse(atStart)
+      var sel = window.getSelection()
+      sel.removeAllRanges()
+      sel.addRange(range)
     } else if (typeof document.body.createTextRange !== "undefined") {
-      var textRange = document.body.createTextRange();
-      textRange.moveToElementText(el);
-      textRange.collapse(atStart);
-      textRange.select();
+      var textRange = document.body.createTextRange()
+      textRange.moveToElementText(el)
+      textRange.collapse(atStart)
+      textRange.select()
     }
-  };
+  }
 }
 
-var placeCaretAtStart = createCaretPlacer(true);
-var placeCaretAtEnd = createCaretPlacer(false);
+var placeCaretAtStart = createCaretPlacer(true)
+var placeCaretAtEnd = createCaretPlacer(false)
 
 function getCaretInfo() {
   const selection = document.getSelection()
@@ -63,7 +63,7 @@ function getCaretInfo() {
   let startCaret = anchorOffset
   for (;;) {
     if (prevElement.previousSibling === null)
-      break;
+      break
 
     prevElement = prevElement.previousSibling
     startCaret += prevElement.textContent.length
@@ -73,7 +73,7 @@ function getCaretInfo() {
   let endCaret = focusOffset
   for (;;) {
     if (prevElement.previousSibling === null)
-      break;
+      break
 
     prevElement = prevElement.previousSibling
     endCaret += prevElement.textContent.length
@@ -87,6 +87,7 @@ function getCaretInfo() {
     type,
     range,
     selection,
+    anchorNode,
     anchorOffset,
     elementWithAnchorNode,
     focusOffset,
@@ -102,74 +103,106 @@ function getCaretInfo() {
   return result
 }
 
-function surroundContents(tagName) {
-  const {
-    activeElement
-  } = document
-
-  if (activeElement === editField) {
-    const {
-      selection,
-      range,
-      anchorOffset,
-      elementWithAnchorNode,
-      elementWithFocusNode
-    } = getCaretInfo()
-    const {
-      startContainer,
-      endContainer
-    } = range
-
-    if (range.toString() === '') 
-      return true
-
-    var surroundFn = (nodes) => {
-      let nodesLen = nodes.length
-
-      for (let i = 0; i < nodesLen; i++) {
-        let node = nodes[i]
-  
-        if (node.tagName === undefined) {
-          let newRange = document.createRange()
-          newRange.selectNode(node)
-          newRange.surroundContents(document.createElement(tagName))
-        } else {
-          if (node.tagName === tagName) 
-            continue
-  
-          surroundFn(node.childNodes)
-        }
-      }
-    }
-
-    if (startContainer === endContainer) {
-      // 이미 B 태그 내부라면 return 
-      if (elementWithAnchorNode.tagName === tagName) 
-        return true;
-
-      range.surroundContents(document.createElement(tagName))
-    } else {
-      let { childNodes } = range.extractContents()
-
-      surroundFn(childNodes)
-      $(elementWithAnchorNode).after(childNodes)
-    }
-  }
-}
-
 function unSurroundContents(tagName) {
 
 }
 
-function surround(tagName) {
-  let isSurrounded = false;
+function getSurroundTargets(tagName) {
+  const caretInfo = getCaretInfo()
+  const {
+    selection,
+    range,
+    anchorOffset,
+    elementWithAnchorNode,
+    elementWithFocusNode
+  } = caretInfo
+  const {
+    startContainer,
+    endContainer
+  } = range
 
-  // 감싸져 있지 않을 경우 Surround
-  if (!isSurrounded)
-    surroundContents(tagName)
+  let surround = []
+  let surroundRange = false
+  let unSurround = []
+  let extractChildNodes = null
+
+  const _getSurroundTarget = (childNodes, tagName) => {
+    let childNodesLen = childNodes.length
+  
+    for (let i = 0; i < childNodesLen; i++) {
+      let childNode = childNodes[i]
+  
+      if (childNode.tagName === undefined) {
+        surround.push(childNode)
+      } else {
+        if (childNode.tagName === tagName) {
+          continue
+        }
+  
+        _getSurroundTarget(childNode.childNodes, tagName)
+      }
+    }
+  }
+
+  if (startContainer === endContainer) {
+    if (elementWithAnchorNode.tagName !== tagName) {
+      surroundRange = true
+    } 
+  } else {
+    let { childNodes } = range.extractContents()
+    extractChildNodes = childNodes
+    _getSurroundTarget(childNodes, tagName)
+  }
+
+  return {
+    surround,
+    surroundRange,
+    unSurround,
+    caretInfo,
+    extractChildNodes
+  }
+}
+
+function _surround(tagName) {
+  const {
+    activeElement
+  } = document
+
+  if (activeElement !== editField || document.getSelection().toString().trim() === '') 
+    return false
+    
+  let isSurrounded = false
+  let surroundTargets = getSurroundTargets(tagName)
+  const {
+    selection,
+    range,
+    anchorOffset,
+    elementWithAnchorNode,
+    elementWithFocusNode
+  } = surroundTargets.caretInfo
+
+  isSurrounded = (surroundTargets.surroundRange || surroundTargets.surround.length > 0) ? false : true
+
+  console.log(`isSurrounded : ${isSurrounded}`)
+
+  // 1개라도 감싸지지 않은 것이 있을 경우 Surround
+  if (!isSurrounded) {
+    if (surroundTargets.surroundRange === true) {
+      range.surroundContents(document.createElement(tagName))
+    } else {
+      surroundTargets.surround.forEach((node, i) => {
+        let newRange = document.createRange()
+        newRange.selectNode(node)
+        newRange.surroundContents(document.createElement(tagName))
+      })
+
+      $(elementWithAnchorNode).after(surroundTargets.extractChildNodes)
+    }
+  }
   // 이미 감싸져 있는 경우 UnSurround
-  else
+  else {
     unSurroundContents(tagName)
+  }
 }
 
 window.onload = function () {
@@ -183,7 +216,7 @@ window.onload = function () {
   function enterKey(e) {
     if (e.which !== 13 || e.shiftKey) return true
 
-    const caretInfo = getCaretInfo();
+    const caretInfo = getCaretInfo()
     const {
       type,
       isLast,
@@ -209,7 +242,7 @@ window.onload = function () {
   }
 
   function bClick() {
-    surround('B')
+    _surround('B')
   }
   
 
@@ -275,3 +308,69 @@ window.onload = function () {
     }
   }
 }
+
+
+
+
+/*
+function _surround(childNodes, tagName) {
+  let childNodesLen = childNodes.length
+
+  for (let i = 0; i < childNodesLen; i++) {
+    let childNode = childNodes[i]
+
+    if (childNode.tagName === undefined) {
+      let newRange = document.createRange()
+      newRange.selectNode(childNode)
+      newRange.surroundContents(document.createElement(tagName))
+    } else {
+      if (childNode.tagName === tagName) 
+        continue
+
+      _surround(childNode.childNodes, tagName)
+    }
+  }
+}
+
+function surroundContents(tagName) {
+  const {
+    activeElement
+  } = document
+
+  if (activeElement === editField) {
+    const {
+      selection,
+      range,
+      anchorNode,
+      anchorOffset,
+      elementWithAnchorNode,
+      elementWithFocusNode
+    } = getCaretInfo()
+    const {
+      startContainer,
+      endContainer
+    } = range
+
+    if (startContainer === endContainer) {
+      // 이미 해당 태그 내부라면 return 
+      if (elementWithAnchorNode.tagName === tagName) 
+        return true
+
+      let { childNodes } = range.extractContents()
+      let childNode = childNodes[0]
+      let newRange = document.createRange()
+      newRange.selectNode(childNode)
+      newRange.surroundContents(document.createElement(tagName))
+      newRange.setStart(anchorNode, anchorOffset)
+      newRange.insertNode(childNodes[0])
+
+      // range.surroundContents(document.createElement(tagName))
+    } else {
+      let { childNodes } = range.extractContents()
+
+      _surround(childNodes, tagName)
+      $(elementWithAnchorNode).after(childNodes)
+    }
+  }
+}
+*/
